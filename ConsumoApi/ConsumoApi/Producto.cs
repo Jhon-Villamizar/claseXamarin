87ff403a3e2c7a53238eab66220076412124acf3
John <jhonvio@hotmail.com>
2018-10-02 21:01:23 -0500

    consumo de api con proyecto de consola

---
+++ ConsumoApi/ConsumoApi/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoApi
+{
+
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public object Imagen { get; set; }
+    }
+
+}
---
+++ ConsumoApi/ConsumoApi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace ConsumoApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            invocarApi();
+        }
+
+        public static void invocarApi()
+        {
+            HttpClient cliente = new HttpClient();
+            //se especifica el sitio al que se realizaran las peticiones
+            cliente.BaseAddress = new Uri("http://mitiendaapis.azurewebsites.net/");
+
+            var request =  cliente.GetAsync("/api/Producto").Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var responseJSON =  request.Content.ReadAsStringAsync().Result;
+                var respuesta = JsonConvert.DeserializeObject<List<Producto>>(responseJSON);
+            }
+        }
+    }
+}
